6a635a5014e2e3d77810521c5ebe80c7c4687e62
Marcelo Sampaio <marcelosampaio@msn.com>
2020-01-21 13:44:49 -0300

    Atualizaçãoes Diversas

--- app/src/main/java/com/marcelosampaio/sgmo_free/act/Sobre.java
+++ app/src/main/java/com/marcelosampaio/sgmo_free/act/Sobre.java
@@ -3,14 +3,21 @@ package com.marcelosampaio.sgmo_free.act;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import com.marcelosampaio.sgmo_free.R;
 
 public class Sobre extends AppCompatActivity {
 
+    TextView linkYouTube;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+
+        linkYouTube = findViewById(R.id.linkYouTube);
+
+        linkYouTube.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
